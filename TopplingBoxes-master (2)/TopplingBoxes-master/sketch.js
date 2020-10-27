53ee6016de25f65a92f6952b0f2a57991ce0d14e
emilesha10 <73525148+emilesha10@users.noreply.github.com>
2020-10-27 14:44:36 +0530

    Add files via upload

---
+++ TopplingBoxes-master (2)/TopplingBoxes-master/sketch.js
@@ -0,0 +1,38 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1,box2,box3,box4;
+var pig1,pig2;
+var log1
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(200,100,50,50);
+    box2 = new Box(200,250,50,50);
+    box3 = new Box(300,260,50,50);
+    box4 = new Box(300,350,50,50);
+    ground = new Ground(200,height,400,20)
+    pig1  =new PIG(200,150,30,30);
+    pig2  =new PIG(200,200,30,30);
+    log1 =new LOG(300,300,100,PI/2)
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    ground.display();
+    pig1.display();
+    pig2.display();
+    log1.display();
+}
